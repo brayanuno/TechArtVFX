52cd29d79814bcfceb3faf97f66a8f779c2e2c83
brayanuno <hbk_596_bdfp@hotmail.com>
2024-10-21 17:00:12 -0700

    checking

---
+++ Assets/BrayanAssets/Scripts/ProjectileSpawn.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ProjectileSpawn : MonoBehaviour
+{
+    // Start is called once before the first
+    // execution of Update after the MonoBehaviour is created
+
+    public GameObject ProjectileToSpawn;
+    public Transform ProjectileSpawnerLocation;
+
+    public float projectileSpeed;
+
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnProjectile()
+    {
+        
+    }
+}
