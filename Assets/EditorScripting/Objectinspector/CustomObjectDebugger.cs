f2cd4820ff1ae749bbcc25846fddcad3f2b7ffc5
brayanuno <hbk_596_bdfp@hotmail.com>
2025-02-19 16:54:13 -0800

    changes

---
+++ Assets/EditorScripting/Objectinspector/CustomObjectDebugger.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class CustomObjectDebugger : EditorWindow
+{
+    private GameObject selectedGameObject;
+    private List<GameObject> filteredGameObjects = new List<GameObject>();
+    
+    //Filters
+    private string searchQuery = "";
+    private string tagFilter = "";
+    private string componentFilter = "";
+    
+    
+    private bool showTransform = true;
+    
+    [MenuItem("Tools/Custom Object Debugger")]
+    public static void ShowWindow()
+    {
+        GetWindow<CustomObjectDebugger>("GameObject Debugger");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        filteredGameObjects.AddRange(GameObject.FindObjectsOfType<GameObject>());
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Custom GameObject Debugger", EditorStyles.boldLabel);
+        GUILayout.Label("Search & Filter", EditorStyles.boldLabel);
+        searchQuery = EditorGUILayout.TextField("Search by Name", searchQuery);
+        tagFilter = EditorGUILayout.TextField("Tag", tagFilter);
+        componentFilter = EditorGUILayout.TextField("Filter by Component", componentFilter);
+        FilteredGameObjects();
+        if (filteredGameObjects.Count > 0)
+        {
+            foreach (GameObject go in filteredGameObjects)
+            {
+                if (GUILayout.Button(go.name))
+                {
+                    selectedGameObject = go;
+                }
+            }
+        }
+        else
+        {
+            GUILayout.Label("No GameObjects match the filter criteria", EditorStyles.boldLabel);
+        }
+    }
+
+    private void FilteredGameObjects()
+    {
+        filteredGameObjects.Clear();
+        GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+        foreach (var go in allGameObjects)
+        {
+            bool matchesSearch = string.IsNullOrEmpty(searchQuery) || go.name.ToLower().Contains(searchQuery.ToLower());
+            bool matchesTag = string.IsNullOrEmpty(tagFilter) || go.CompareTag(tagFilter);
+            bool matchesComponent = string.IsNullOrEmpty(componentFilter) || go.CompareTag(componentFilter);
+
+            if (matchesSearch && matchesTag && matchesComponent)
+            {
+                filteredGameObjects.Add(go);
+            }
+        }
+    }
+    
+
+    // Update is called once per frame
+}
--- Assets/EditorScripting/TextureOptimizer/TextureOptimizer.cs
+++ Assets/EditorScripting/TextureOptimizer/TextureOptimizer.cs
@@ -2,7 +2,7 @@ using Unity.Mathematics.Geometry;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(TextureImporter),  true)]
+/*[CustomEditor(typeof(TextureImporter),  true)]
 public class TextureOptimizer : Editor
 {
     
@@ -11,22 +11,49 @@ public class TextureOptimizer : Editor
         DrawDefaultInspector();
         if (GUILayout.Button("Optimize Texture"))
         {
-            string path = AssetDatabase.GetAssetPath(Selection);
-            //Getting the actually importer
-            TextureImporter importer = (TextureImporter)TextureImporter.GetAtPath(path);
-            int height;
-            int width;
-            importer.GetSourceTextureWidthAndHeight(out height, out width);
-            int maxDimension = Mathf.Max(height, width);
+            SetMaxTextureSizeOptimizer();
+        }#1#
 
-            TextureImporterSettings textureImporterSettings = new TextureImporterSettings();
-            importer.ReadTextureSettings(textureImporterSettings);
-            textureImporterSettings.maxTextureSize = (int)Mathf.Pow(2, (int)Mathf.Log(maxDimension -1,2) + 1);
-            importer.SetTextureSettings(textureImporterSettings);
-            EditorUtility.SetDirty(importer);
-            importer.SaveAndReimport();
+    void SetMaxTextureSizeOptimizer()
+    {
+        //Getting the path of selectedObject
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        Debug.Log("Optimized Texture");
+        Debug.Log(path);
+        //Getting the actually importer
+        TextureImporter importer = (TextureImporter)TextureImporter.GetAtPath(path);
+            
+        int height;
+        int width;
+            
+        //storing maxDimension on height and width
+        importer.GetSourceTextureWidthAndHeight(out height, out width);
+        //get max Dimension
+        int maxDimension = Mathf.Max(width, height);
+            
+        //creating TextureImporterSettings instance
+        TextureImporterSettings textureImporterSettings = new TextureImporterSettings();
+        
+        //read Textures from importer
+        importer.ReadTextureSettings(textureImporterSettings);
+        //adjusting importSetting to new Dimension
+        textureImporterSettings.maxTextureSize = CalculateMaxTextureSizeOptimizer(maxDimension);
+         
+        //save importer
+        importer.SetTextureSettings(textureImporterSettings);
+        EditorUtility.SetDirty(importer);
+        importer.SaveAndReimport();    
+
         };
     }
+
+    private int  CalculateMaxTextureSizeOptimizer(int maxDimension)
+    {
+        return (int)Mathf.Pow(2, (int)Mathf.Log(maxDimension -1,2) + 1);
+        
+    }
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,5 +64,5 @@ public class TextureOptimizer : Editor
     void Update()
     {
         
-    }
-}
+    }*/
+/*}*/
